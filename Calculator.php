0b5b06991565a7bcf2b4d114ef9a9a3bd544d5fc
HoXuanTy <116275520+HoXuanTy@users.noreply.github.com>
2023-03-13 01:19:09 -0700

    Add files via upload

---
+++ Calculator.php
@@ -0,0 +1,10 @@
+<?php
+  class Calculator{
+    public function sum ($a, $b) {
+        return $a + $b;
+    }
+    public function minus (int $a, int $b) : int {
+        return $a - $b;
+    }
+}
+?>
